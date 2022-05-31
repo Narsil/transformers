2fe54dfd4970debd10c07cda6720a9802a991d0e
younesbelkada <younesbelkada@gmail.com>
2022-05-31 14:48:41 +0200

    add accelerate compatibility
    Co-authored-by: Nicolas Patry <Narsil@users.noreply.github.com>

--- src/transformers/models/bloom/modeling_bloom.py
+++ src/transformers/models/bloom/modeling_bloom.py
@@ -560,6 +560,7 @@ class BloomPreTrainedModel(PreTrainedModel):
     base_model_prefix = "transformer"
     is_parallelizable = True
     supports_gradient_checkpointing = True
+    _no_split_modules = ["BloomBlock"]
 
     def __init__(self, *inputs, **kwargs):
         super().__init__(*inputs, **kwargs)
